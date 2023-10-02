f852f7e82bf324d3d2936300c2965f4e48c52a25
Mike Szczys <szczys@hotmail.com>
2023-10-01 20:33:49 -0500

    Power OLED from p1.06

--- app/src/main.c
+++ app/src/main.c
@@ -7,6 +7,8 @@
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/devicetree.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/init.h>
 #include <zephyr/settings/settings.h>
 
 #include <zephyr/logging/log.h>
@@ -16,6 +18,18 @@ LOG_MODULE_REGISTER(zmk, CONFIG_ZMK_LOG_LEVEL);
 #include <zmk/kscan.h>
 #include <zmk/display.h>
 #include <drivers/ext_power.h>
+
+#if DT_NODE_EXISTS(DT_NODELABEL(oled_pwr))
+static int oled_pwr_init(const struct device *)
+{
+    const struct gpio_dt_spec oled_pwr = GPIO_DT_SPEC_GET(DT_NODELABEL(oled_pwr), gpios);
+    gpio_pin_configure_dt(&oled_pwr, GPIO_OUTPUT_ACTIVE);
+
+    return 0;
+}
+
+SYS_INIT(oled_pwr_init, PRE_KERNEL_1, 0);
+#endif
 
 void main(void) {
     LOG_INF("Welcome to ZMK!\n");
